5a88f72f79cb0601548ed30619466733452e688e
Денис Тавабилов <dagger1233@gmail.com>
2023-08-17 07:43:28 +0500

    tuples

--- app.ts
+++ app.ts
@@ -1,1 +1,34 @@
-const skill: [number, string] = [1, 'dev']
+/* Запрос */
+// {
+// 	"topicId": 5,
+// 	"status": "published" // "draft", "deleted"
+// }
+/* Ответ */
+// [
+// 	{
+// 		"question": "Как осуществляется доставка?",
+// 		"answer": "быстро!",
+// 		"tags": [
+// 			"popular",
+// 			"new"
+// 		],
+// 		"likes": 3,
+// 		"status": "published"
+// 	}
+// ]
+
+enum QuestionStatus {
+    Published = 'published',
+    Draft = 'Draft',
+    Deleted = 'Deleted'
+}
+
+// Промис не стал дописывать
+async function getFaqs(req:{topicId: number, status: QuestionStatus}):Promise<{question: string}> {
+	const res = await fetch('/faqs', {
+		method: 'POST',
+		body: JSON.stringify(req)
+	});
+	const data = await res.json();
+	return data;
+}
