5081bce7fe492897aa88bd9a423cd7ad95730a35
Денис Тавабилов <dagger1233@gmail.com>
2023-08-21 08:33:11 +0500

    generics

--- app.ts
+++ app.ts
@@ -1,14 +1,35 @@
-function logMiddleware<T>(data: T): T {
-  console.log(data);
-  return data;
+// function logMiddleware<T>(data: T): T {
+//   console.log(data);
+//   return data;
+// }
+
+// const res = logMiddleware<number>(10);
+// const res2 = logMiddleware<string>("q");
+
+// function getSplitedHaldf<T>(data: Array<T>): Array<T> {
+//   const l = data.length / 2;
+//   return data.splice(0, l);
+// }
+
+// getSplitedHaldf([32,323,1233])
+
+function toString<T>(value: T): string | undefined {
+  if (Array.isArray(value)) {
+    return value.toString();
+  }
+
+  switch (typeof value) {
+    case "string":
+      return value;
+    case "number":
+    case "symbol":
+    case "bigint":
+    case "boolean":
+    case "function":
+      return value.toString();
+    case "object":
+      return JSON.stringify(value);
+    default:
+      return undefined;
+  }
 }
-
-const res = logMiddleware<number>(10);
-const res2 = logMiddleware<string>("q");
-
-function getSplitedHaldf<T>(data: Array<T>): Array<T> {
-  const l = data.length / 2;
-  return data.splice(0, l);
-}
-
-getSplitedHaldf([32,323,1233])
