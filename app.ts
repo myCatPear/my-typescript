908881e4dc59d8d928bab4c48e2a5238d52c8071
Денис Тавабилов <dagger1233@gmail.com>
2023-08-22 07:57:07 +0500

    key of

--- app.ts
+++ app.ts
@@ -1,35 +1,15 @@
-// function logMiddleware<T>(data: T): T {
-//   console.log(data);
-//   return data;
-// }
-
-// const res = logMiddleware<number>(10);
-// const res2 = logMiddleware<string>("q");
-
-// function getSplitedHaldf<T>(data: Array<T>): Array<T> {
-//   const l = data.length / 2;
-//   return data.splice(0, l);
-// }
-
-// getSplitedHaldf([32,323,1233])
-
-function toString<T>(value: T): string | undefined {
-  if (Array.isArray(value)) {
-    return value.toString();
-  }
-
-  switch (typeof value) {
-    case "string":
-      return value;
-    case "number":
-    case "symbol":
-    case "bigint":
-    case "boolean":
-    case "function":
-      return value.toString();
-    case "object":
-      return JSON.stringify(value);
-    default:
-      return undefined;
-  }
+interface IUser {
+  name: string;
+  age: number;
 }
+
+function getValue<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
+}
+
+const user: IUser = {
+  age: 11,
+  name: "Da",
+};
+
+const userName = getValue(user, "name");
