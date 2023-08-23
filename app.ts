8e6352fbb808a90f57d513ed9a3a606c855f8d07
Денис Тавабилов <dagger1233@gmail.com>
2023-08-23 07:46:40 +0500

    indexed access types

--- app.ts
+++ app.ts
@@ -1,7 +1,33 @@
-const user = {
-  name: 'denis',
-  age: 33
+interface Role {
+  name: string;
 }
 
-type keyOFUser = keyof typeof user;
+interface Permission {
+  endDate: Date;
+}
 
+interface User {
+  name: string;
+  roles: Role[];
+  permission: Permission;
+}
+
+const user: User = {
+  name: "Denis",
+  roles: [],
+  permission: {
+    endDate: new Date(),
+  },
+};
+
+const nameUSer = user["name"];
+const roleNames = "roles";
+
+type rolesType = User["roles"];
+type rolesType2 = User[typeof roleNames];
+
+type roleType = User["roles"][number];
+type dateType = User["permission"]["endDate"];
+
+const roles = ["admin", "user"] as const;
+type roleTypes = (typeof roles)[number];
